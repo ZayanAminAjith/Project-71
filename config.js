c85466a36fdba6131ac86c8ca1f1a0ed18d6ac9d
ZayanAminAjith <96104239+ZayanAminAjith@users.noreply.github.com>
2022-12-07 16:10:51 +0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+//Paste your firebaseConfig here
+    const firebaseConfig = {
+        apiKey: "AIzaSyAcVjvQza3fnTjvuHgn6yZq-nV_bdV2ZRA",
+        authDomain: "project-71-4b35d.firebaseapp.com",
+        projectId: "project-71-4b35d",
+        storageBucket: "project-71-4b35d.appspot.com",
+        messagingSenderId: "978098716968",
+        appId: "1:978098716968:web:f7a88f1f8b6d47f6e2901d"
+    };
+  
+//
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
